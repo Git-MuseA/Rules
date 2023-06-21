463329b23e959ed0bdc9ec3e0f2fa3be671f7b8a
Yiben Huang <62163340+yibenhuang@users.noreply.github.com>
2023-06-21 15:47:30 +0800

    Update the script

--- update_rules.py
+++ update_rules.py
@@ -1,14 +1,35 @@
+"""Update the rule provider files defined in the 'config.yaml' file.
+
+It downloads the files from the URLs given in the config, saves them to disk, and then generates a 
+new rules file by concatenating the downloaded files with the existing rules file.
+"""
+from pathlib import Path
+
 import requests
 import yaml
-from yaml.loader import SafeLoader
 
-config_dict = yaml.load(open("myconfig.yaml", "r"), Loader=SafeLoader)
+# Load the Clash configuration file
+config_file_path = Path("config.yaml")
 
-rule_providers_dict = config_dict["rule-providers"]
-rule_urls = [rule_providers_dict[k]["url"] for k in rule_providers_dict]
-rule_local_paths = [rule_providers_dict[k]["path"] for k in rule_providers_dict]
+with config_file_path.open() as f:
+    config = yaml.safe_load(f)
 
-for url, path in dict(zip(rule_urls, rule_local_paths)).items():
-    with open(path, "wb") as f:
-        content = requests.get(url, stream=True).content
-        f.write(content)
+# Check if `rule_provider` directory exists
+rule_provider_dir = Path.cwd().joinpath("rule_provider")
+if not rule_provider_dir.exists():
+    rule_provider_dir.mkdir()
+
+# Loop through each rule provider
+for provider in config["rule-providers"]:
+    # Get the URL and path for this provider
+    url = config["rule-providers"][provider]["url"]
+    path = rule_provider_dir.joinpath(Path(config["rule-providers"][provider]["path"]).name)
+
+    # Download the rule provider file
+    if path.suffix in (".txt", ".yaml"):
+        print(f"Downloading {url} to {path}")
+        r = requests.get(url, timeout=5)
+        with path.open(mode="w") as f:
+            f.write(r.text)
+    else:
+        print(f"Skipping {provider}")
